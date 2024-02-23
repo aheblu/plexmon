391c5df300b9b04eec0f2172c691f42eb54513c1
my <my@xoxo>
2024-02-23 11:14:57 +0200

    improved error handling

--- plexmon.sh
+++ plexmon.sh
@@ -4,19 +4,22 @@
 #
 # info: notifies plex when a library directory changes
 #
-# requires: 
+# requires:
 #   bash 5.2.15
-#   curl 
+#   curl
 #   fswatch 1.13
 #   xmllint: using libxml version 21004
 #   jq 1.7
 
 SCRIPT_NAME="plexmon"
-SCRIPT_VERSION="v1.0.0"
+SCRIPT_VERSION="v1.1.0"
 SCRIPT_DESCR="Plex library auto-updater for *BSD"
 PID_FILE="/var/run/$SCRIPT_NAME.pid"
 CONFIG_FILE="/usr/local/etc/$SCRIPT_NAME.conf"
+CONFIG_VARS="PLEX_IP PLEX_TOKEN PLEX_PORT LOG_LEVEL MON_LATENCY"
+DEPENDENCIES="bash curl xmllint fswatch jq"
 LOG_FILE="/var/log/$SCRIPT_NAME.log"
+
 
 PARAM1=$1
 
@@ -28,7 +31,7 @@ IS_RUNNING=0
 if [ -f "$PID_FILE" ]; then
     PID_IN_FILE=$(<$PID_FILE)
     echo "$PID_FILE exists: $PID_IN_FILE"
-    # check if really running    
+    # check if really running
     ps -p $PID_IN_FILE > /dev/null 2>&1
     if [ $? -eq 0 ]; then
         echo "fswatch with PID $PID_IN_FILE is running."
@@ -42,47 +45,26 @@ if [ -f "$PID_FILE" ]; then
     fi
 fi
 
-script_stop(){
-    message "Stopping $SCRIPT_NAME..." "BOTH"
-    kill -SIGHUP $PID_IN_FILE 
-    [ $? -ne 0 ] && message "Error killing PID $PID_IN_FILE. Not enough permissions or already dead." "BOTH" 
+fswatch_stop(){
+    message "Stopping fswatch..." "BOTH"
+    kill -SIGHUP $PID_IN_FILE
+    [ $? -ne 0 ] && message "Error killing PID $PID_IN_FILE. Not enough permissions or already dead." "BOTH"
     [ -f $PID_FILE ] && rm $PID_FILE
     [ $? -ne 0 ] && message "Error removing $PID_FILE" "BOTH" && exit
-    message "$SCRIPT_NAME clean stop."
+    message "fswatch clean stop."
 }
-
-# REQUIREMENT CHECK - CONFIG
-if [ -f "$CONFIG_FILE" ]; then
-    . $CONFIG_FILE
-else
-    echo "Can not find config file: $CONFIG_FILE"
-    exit 1
-fi
-
-# REQUIREMENT CHECK - SOFTWARE
-check_prog (){
-    which "$1" > /dev/null
-    if [ $? -ne 0 ]; then
-        echo "$1 not found in PATH. Aborting."
-        exit 1
-    fi
-}
-
-check_prog "bash"
-check_prog "curl"
-check_prog "xmllint"
-check_prog "fswatch"
-check_prog "jq"
 
 # LOG
+message (){
+    local msg=$1
+    local log=$2
+    local log_msg="$(date '+ %y/%m/%d% - %H:%M:%S') $msg"
 
-message (){    
-    msg=$1
-    log=$2
-    log_msg="$(date '+ %y/%m/%d% - %H:%M:%S') $msg"
-    
+    # if LOG_LEVEL is undefined just print the message
+    [ -z $LOG_LEVEL ] && echo $log_msg && return
+
     case $log in
-    LOG)        
+    LOG)
         [ $LOG_LEVEL -eq 1 ] && echo $log_msg >> $LOG_FILE
         ;;
     BOTH)
@@ -91,11 +73,10 @@ message (){
         ;;
     *)
         echo $log_msg
-    esac    
+    esac
 }
 
-# PARAMETER CHECK
-
+# COMMAND LINE CHECK
 case $PARAM1 in
 --start)
     [ $IS_RUNNING -eq 1 ] && message "Start: $SCRIPT_NAME already running." && exit
@@ -103,58 +84,102 @@ case $PARAM1 in
 
 --stop)
     [ $IS_RUNNING -eq 0 ] && message "Stop: $SCRIPT_NAME not running."
-    [ $IS_RUNNING -eq 1 ] && script_stop && message "Stop: $SCRIPT_NAME terminated."
+    [ $IS_RUNNING -eq 1 ] && fswatch_stop && message "Stop: $SCRIPT_NAME terminated."
     exit
     ;;
 
 --restart)
-    [ $IS_RUNNING -eq 1 ] && script_stop
+    [ $IS_RUNNING -eq 1 ] && fswatch_stop
     [ $IS_RUNNING -eq 0 ] && message "Restart: $SCRIPT_NAME not running. Starting..."
     ;;
 
 *)
     if [ -z "$PARAM1" ]; then
         if [ $IS_RUNNING -eq 1 ]; then
-            message "$SCRIPT_NAME is running."                    
+            message "$SCRIPT_NAME is running."
         else
-            message "$SCRIPT_NAME not running. Use \"$SCRIPT_NAME --start\""            
+            message "$SCRIPT_NAME not running. Use \"$SCRIPT_NAME --start\""
         fi
     else
-        message "Valid parameters are: --start, --stop, --restart"          
+        message "Valid parameters are: --start, --stop, --restart"
     fi
     exit
     ;;
 esac
 
-# FUNCTIONS
+# REQUIREMENT CHECK - config file
+if [ -f "$CONFIG_FILE" ]; then
+    . $CONFIG_FILE
+else
+    fswatch_stop
+    echo "$CONFIG_FILE not present."
+    echo "$SCRIPT_NAME terminated." && exit 1
+fi
 
-# info: gets plex library id's for a path (there can be more libraries containing same path)
-# return: $path_ids (array)   
-id_for_path (){
-     
-    if [ -z $1 ]; then
-        message "Should not happen: id_for_path - Null parameter." "LOG"; exit 1   
+# REQUIREMENT CHECK - config variables
+for var in $CONFIG_VARS; do
+    if [ -z ${!var} ]; then
+        echo "$var not set in $CONFIG_FILE"
+        echo "Checkout $SCRIPT_NAME.conf.sample for reference."
+        exit 1
     fi
-    
+done
+
+# REQUIREMENT CHECK - dependencies
+for prog in $DEPENDENCIES; do
+    which "$prog" > /dev/null
+    if [ $? -ne 0 ]; then
+        echo "$prog not found in PATH. Aborting."
+        exit 1
+    fi
+done
+
+# REQUIREMENT CHECK - Plex Server connection check
+resp=$(curl -s "http://$PLEX_IP:$PLEX_PORT/library/sections/?X-Plex-Token=$PLEX_TOKEN")
+error=$?
+[ $error -eq 7 ] && message "ERROR: Can not connect to Plex Server, wrong ip/port or server offline." "BOTH" && exit 1
+[ $error -eq 52 ] && message "ERROR: Plex Server secure connections set to Require. Change setting to Preferred or use 127.0.0.1 as server ip in conf file." "BOTH" && exit 1
+[ $error -ne 0 ] && message "ERROR: Can not connect to Plex Server! Unknown error code." && exit 1
+[ -z "$resp" ] && message "ERROR: Empty response from server! Wrong port or unkonwn error." "BOTH" && exit 1
+[[ $resp =~ "401 Unauthorized" ]] && message "ERROR: Plex Token unauthorized !" && exit 1
+unset resp
+unset error
+
+# PLEX FUNCTIONS
+
+# id_for_path
+# info: gets plex library id's for a path (there can be more libraries containing same path)
+# return: $path_ids (array)
+id_for_path (){
+
+    if [ -z "$1" ]; then
+        message "WARNING in id_for_path() - No path to search for." "LOG"
+        path_ids=()
+        return
+    fi
+
     media_path=$1
 
-    resp="$(curl -s "http://$PLEX_IP:$PLEX_PORT/library/sections/?X-Plex-Token=$PLEX_TOKEN" |\
-            xmllint --xpath "MediaContainer/Directory/Location[@path='$media_path']/../@key" -)"    
-    if [ $? -ne 0 ] || [ -z "$resp" ]; then
-        message "Server error: id_for_path - Error getting data from server for $media_path." "LOG"
-        message "Server response following:" "LOG"
-        message $resp "LOG"
-        message "Server response end." "LOG"    
+    local xml_resp="$(curl -s "http://$PLEX_IP:$PLEX_PORT/library/sections/?X-Plex-Token=$PLEX_TOKEN")"
+    [ $? -ne 0 ] && message "WARNING in id_for_path(): Error getting data from server for $media_path." "LOG"
+
+    local xml_parse="$(echo $xml_resp | xmllint --xpath "MediaContainer/Directory/Location[@path='$media_path']/../@key" -)"
+    if [ $? -ne 0 ] || [ -z "$xml_parse" ]; then
+        message "WARNING in id_for_path(): xmllint error parsing server response for $media_path." "LOG"
+        message "xmllint response:" "LOG"
+        message "[ $xml_parse ]" "LOG"
+        message "xmllint response end." "LOG"
     fi
 
     unset path_ids
-    readarray path_ids <<< "$(printf '%s' "$resp")"
+    readarray path_ids <<< "$(printf '%s' "$xml_parse")"
 
-    for i in ${!path_ids[@]}; do        
-        eval ${path_ids[$i]} # creates var $key        
+    for i in ${!path_ids[@]}; do
+        eval ${path_ids[$i]} # creates var $key
         path_ids[$i]=$key
         message "Found ID:${path_ids[$i]}" "LOG"
-    done    
+        unset key
+    done
 }
 
 # get_plex_paths: get plex library paths from server
@@ -162,33 +187,41 @@ id_for_path (){
 # return: plex_paths_escaped (escaped)
 get_plex_paths (){
 
-    if [ -z "$PLEX_IP" ] || [ -z "$PLEX_PORT" ] || [ -z "$PLEX_TOKEN" ];then
-        message "Config check failed. Some config variables empty." "BOTH";exit 1
-    fi
-
-    message "Getting plex data from $PLEX_IP:$PLEX_PORT" "BOTH"
+    message "Getting plex data from $PLEX_IP:$PLEX_PORT..." "BOTH"
     message "" # echo blank line
-
-    resp="$(curl -s "http://$PLEX_IP:$PLEX_PORT/library/sections/?X-Plex-Token=$PLEX_TOKEN" |\
-            xmllint --xpath "MediaContainer/Directory/Location/@path" -)"
-    if [ $? -ne 0 ] || [ -z "$resp" ]; then    
-         message "get_plex_paths: Error getting data from server." "BOTH"
+    local xml_resp="$(curl -s "http://$PLEX_IP:$PLEX_PORT/library/sections/?X-Plex-Token=$PLEX_TOKEN")"
+    if [ $? -ne 0 ]; then
+        message "ERROR in get_plex_paths(): curl error getting data from server !" "BOTH"
+        message "Nothing to do without plex paths. Exiting..." "BOTH"; exit 1
     fi
-    
+
+    if [ -z "$xml_resp" ]; then
+        message "ERROR in get_plex_paths(): server sent empty response !"
+        message "Nothing to do without plex paths. Exiting..." "BOTH"; exit 1
+    fi
+
+    local xml_parse="$( echo ${xml_resp} | xmllint --xpath "MediaContainer/Directory/Location/@path" -)"
+    if [ $? -ne 0 ] || [ -z "$xml_parse" ]; then
+         message "ERROR in get_plex_paths(): xmllint error parsing server response !" "BOTH"
+         message "get_plex_paths: xmllint response:" "BOTH"
+         message "[ ${xml_parse} ]" "BOTH"
+         message "Nothing to do without plex paths. Exiting..." "BOTH"; exit 1
+    fi
+
     unset plex_paths
-    readarray -t plex_paths <<< "$(printf '%s' "$resp")" # important to use quotes for printf   
+    readarray -t plex_paths <<< "$(printf '%s' "${xml_parse}")" # important to use quotes for printf
 
     # plex_paths element data: path="/some/path"
     for i in ${!plex_paths[@]}; do
         #echo "Plex path found: ${plex_paths[$i]}"
         eval ${plex_paths[$i]} # creates var $path
-        plex_paths[$i]=$path
-        escaped_path="$(echo $path | sed -e 's/\ /\\ /g')/"  # escape spaces, with trailing slash        
+        plex_paths[$i]=${path}
+        local escaped_path="$(echo $path | sed -e 's/\ /\\ /g')/"  # escape spaces, with trailing slash
         message "Found plex path: ${plex_paths[$i]}" "BOTH"
         plex_paths_escaped="$plex_paths_escaped $escaped_path"
-    done    
+    done
     echo ""
-    
+
     #trim spaces
     plex_paths_escaped=`echo $plex_paths_escaped | sed -e 's/^[[:space:]]*//'`
 }
@@ -196,83 +229,79 @@ get_plex_paths (){
 # info: get the plex path contained in param1
 # return: find_plex_path_resp
 find_plex_path (){
-    
-    if [ -z "$1" ]; then
-        message "Script error: find_plex_path - Null parameter."; exit 1
-    fi
-    check_path=$1 
 
-    found_path=""
+    local check_path=$1
+    if [ -z "$check_path" ]; then
+        message "ERROR in find_plex_path() - Null parameter. Script error. Shutting down." "BOTH"; fswatch_stop; exit 1
+    fi
+
+    local found_path=""
+    local slash_number=0
+    local max_slash=0
     find_plex_path_resp=""
-    slash_number=0
-    max_slash=0
-    
+
     for i in ${!plex_paths[@]}; do
         # if begins with plex_paths[i]
         if [[  $check_path =~ ^${plex_paths[i]}.* ]]; then
             # if there are nested paths
             # match the plex path with max depth
             found_path=${plex_paths[$i]}
-            slash_number="$(echo $found_path | grep -o \/ | wc -l)"            
+            slash_number="$(echo $found_path | grep -o \/ | wc -l)"
             if (( slash_number > max_slash )); then
                 find_plex_path_resp=$found_path
                 max_slash=$slash_number
-            fi            
+            fi
         fi
     done
     if [ -z "$find_plex_path_resp" ]; then
-        message "find_plex_path: no plex path found for $check_path" "LOG"
+        message "WARNING in find_plex_path(): no plex path found for $check_path" "LOG"
+    else
+        message "Plex path found: $find_plex_path_resp" "LOG"
     fi
 }
 
 plex_partial_scan (){
-    scan_folder=$1
-    scan_id=$2
-    
+    local scan_folder=$1
+    local scan_id=$2
+
     message "Start partial scan: $scan_folder with id: $scan_id" "LOG";
     scan_folder=$(echo $scan_folder | jq "@uri" -jRr) # URL-encode scan folder
     curl -s "http://$PLEX_IP:$PLEX_PORT/library/sections/$scan_id/refresh?path=$scan_folder&X-Plex-Token=$PLEX_TOKEN"
-    if [ $? -ne 0 ]; then    
-         message "plex_partial_scan: Error getting data from server." "LOG"
-         #exit 1
-    fi
+    [ $? -ne 0 ] && \
+        message "WARNING in plex_partial_scan(): curl could not get data from server." "LOG"
 }
 
 plex_library_scan (){
-    scan_id=$1
+    local scan_id=$1
 
     message "Start library scan with id: $scan_id" "LOG"
-    
+
     curl -s "http://$PLEX_IP:$PLEX_PORT/library/sections/$scan_id/refresh?X-Plex-Token=$PLEX_TOKEN"
-    if [ $? -ne 0 ]; then    
-         message "plex_library_scan: Error getting data from server." "LOG"
-         #exit 1
-    fi
+    [ $? -ne 0 ] && \
+        message "WARNING in plex_library_scan(): curl could not get data from server." "LOG"
+
 }
 
 # SCRIPT
 
 get_plex_paths # returns plex_paths_escaped
 
-if [ $? -ne 0 ]; then    
-    message "Startup: Unknown error. Abort." "BOTH"; exit 1
-fi
-
 message "Starting fswatch..." "BOTH"
 
 # start fswatch in background
 fswatch -0rd -l $MON_LATENCY $plex_paths_escaped 2>error.log |\
- while read -d "" event 
-  do 
-    message "Path changed: $event" "LOG"
-    find_plex_path "$event"
-    message "Plex path found: $find_plex_path_resp" "LOG"
-    id_for_path $find_plex_path_resp    
-    for i in ${!path_ids[@]}; do        
-        plex_partial_scan "$event" "${path_ids[$i]}"
-        #plex_library_scan ${path_ids[$i]}
-    done    
-  done &
+    while read -d "" event; do
+        if [[ $event =~ "/" ]]; then # if event contains a path
+            message "fswatch: Path changed: $event" "LOG"
+            find_plex_path "$event" # returns find_plex_path_resp
+            id_for_path $find_plex_path_resp # returns path_ids array
+            for i in ${!path_ids[@]}; do
+                plex_partial_scan "$event" "${path_ids[$i]}"
+            done
+        else
+            message "WARNING in fswatch: Event is not a path. Event: [ $event ]" "BOTH"
+        fi
+    done &
 
 # wait for fswatch to start
 i=1
@@ -282,7 +311,7 @@ while [ -z "$FSWATCH_PID" ] && [ $i -lt $retry ]; do
     echo "[$i - $retry] Waiting for fswatch..."
     sleep 1
     ((i++))
-    FSWATCH_PID=$(pgrep -n fswatch)    
+    FSWATCH_PID=$(pgrep -n fswatch)
 done
 
 [ -z $FSWATCH_PID ] && message "Error starting fswatch." "BOTH" && exit
